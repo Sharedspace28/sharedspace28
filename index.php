c910e2974e12788283f70c51c8bfe73658f0ac93
sharedspace <71169185+Sharedspace28@users.noreply.github.com>
2020-09-13 18:37:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -583,10 +583,10 @@ Providing readers with good content according to your topic <br>
 
   
 <!-- <?php  //foreach ($select_information as $key => $category): ?> -->
- <div class="container">
+ 
 <div class="col-xs-12">
 
-  <footer class="page-footer font-small footer btn-secondary" style="width:500px% background-color: 48;">
+  <footer class="page-footer font-small footer btn-secondary" style="width:1000px background-color: 48;">
   <!-- Copyright -->
 
   <div class="footer-copyright text-center py-3">
@@ -594,7 +594,7 @@ Providing readers with good content according to your topic <br>
   </div>
 
 </footer>
-</div>
+
 	 </div>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
